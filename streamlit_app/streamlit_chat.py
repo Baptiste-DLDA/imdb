5061cdb8652af64753061cd9ce347de14b238043
Mathéo Eymar <matheocarle59@gmail.com>
2025-05-27 01:58:45 +0200

    Version finale

--- streamlit_app/streamlit_chat.py
+++ streamlit_app/streamlit_chat.py
@@ -13,7 +13,7 @@ from translations import translations
 
 st.set_page_config(layout="wide")
 
-key=""
+key="AIzaSyCMLInEu3ro_FhZobk9PZWGPSnyFVO_C8w"
 
 if "explications" not in st.session_state:
     st.session_state.explications = []
